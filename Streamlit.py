d982eb5444294ce4a44cba83c5b720f1ebfdf4a9
Mont3ll <124686740+Mont3ll@users.noreply.github.com>
2023-02-14 01:15:45 +0300

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import pandas as pd
-import plotyl.express as px
+import plotly.express as px
 
-st.set_page_config(page_title="Data 2007")
+st.set_page_config(page_title="Data 2007",
+                   page_icon=":bar_chart:",
+                   layout="wide")
 
 data=pd.read_csv("gapminder_with_codes.csv")
 data_2007=data[data['year']==2007]
-data_2007
+
+st.sidebar.header("Filter:")
+continent=st.sidebar.multiselect(
+         "Select the continent:",
+         options=data_2007["continent"].unique(),
+         default=data_2007["continent"].unique()
+)
+data_2007_selection=data_2007.query(
+         "continent==@continent"
+)
+st.dataframe(data_2007_selection)
