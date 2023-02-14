c4a584ed81e4054a1ebe1e7f26e6014a0258d074
Mont3ll <124686740+Mont3ll@users.noreply.github.com>
2023-02-14 21:26:19 +0300

    Add files via upload
    Interactive plot

--- Streamlit.py
+++ Streamlit.py
@@ -2,9 +2,25 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+def data_header(dataframe):
+    st.header('Data Header')
+    st.write(data_2007_selection.head())
+
+def interactive_plot(dataframe):
+    x_axis=st.selectbox('Select x-axis value', options=data_2007.columns)
+    y_axis=st.selectbox('Select y-axis value', options=data_2007.columns)
+    col=st.color_picker('Select a plot colour')
+
+    plot=px.scatter(data_2007_selection, x=x_axis, y=y_axis)
+    plot.update_traces(marker=dict(color=col))
+    st.plotly_chart(plot)
+
 st.set_page_config(page_title="Data 2007",
                    page_icon=":bar_chart:",
                    layout="wide")
+
+st.title("Data 2007")
+st.text('GDP, Life Expectancy and Populaton for various countries for the year 2007.')
 
 data=pd.read_csv("gapminder_with_codes.csv")
 data_2007=data[data['year']==2007]
@@ -18,4 +34,15 @@ continent=st.sidebar.multiselect(
 data_2007_selection=data_2007.query(
          "continent==@continent"
 )
-st.dataframe(data_2007_selection)
+options=st.sidebar.radio(
+         "Pages",
+         options=['Home',
+         'Data header',
+         'Interactive plot']
+)
+if options=='Home':
+    st.dataframe(data_2007_selection)
+elif options=='Data header':
+    data_header(data_2007)
+elif options=='Interactive plot':
+    interactive_plot(data_2007)
